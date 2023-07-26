4bed0c7eccbf85057b05afb12905c7f48b75c5c7
myspace8 <comfyspace8@gmail.com>
2023-07-26 02:49:05 -0700

    increased overlay height

--- pages/index.js
+++ pages/index.js
@@ -191,7 +191,7 @@ export default function Home() {
               top: 0,
               left: 0,
               width: '100%',
-              height: '100%',
+              height: '100vh',
               background: 'rgba(0, 0, 0, 0.5)',
             }}
           ></div>
